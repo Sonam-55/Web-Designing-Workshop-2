7dea88115b24b21026aae656da42241047969314
Sonam Yadav <yadavsonam23032007>
2026-04-06 16:56:46 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+console.log("Hello World");
+document.write("Hello World");
+
+    var num=7;
+    console.log(num);
+    console.log(typeof(num));
+    num=false;
+    console.log(num);
+    console.log(typeof(num));
+    num="Trishya";
+    console.log(num);
+    console.log(typeof(num));
+    num=null;
+    console.log(num);
+    console.log(typeof(num));
+    num=undefined;  
+    console.log(num);
+    console.log(typeof(num));
+    var numberArray=[1,2,3,4,5];
+
+    var animals=new Array("Dog","Cat","Elephant");
+    //object
+    var person={
+        name:"Trishya",
+        age:20,
+        title:"Student"
+    }
+    const sym1=Symbol(4);
+    console.log(sym1);
+
+    const sym2=Symbol(4);
+    console.log(sym2);
+    if(sym1===sym2){
+        console.log("true");
+    }
+    else{      
+          console.log("false");  
+    } 
+    var a=10;;
+    var b=20;
+    var c=30;
+    var linebreak="<br>";
+    document.write("a+b+c=");
+    result=a+b+c;
+    document.write(result);
+    document.write(linebreak);
+    document.write("a-b-c=");
+    result=a-b-c;
+        
+    result=a-b-c;
+    document.write(result);
+    document.write(linebreak);
+    document.write("a*b*c=");   
+    result=a*b*c;
+    document.write(result);
+    document.write(linebreak);  
+    document.write("a/b=");
+    result=a/b;
+    document.write(result);
+    document.write(linebreak);
+    document.write("a%b=");
+    result=a%b; 
+    document.write(result); 
+    document.write(linebreak);
